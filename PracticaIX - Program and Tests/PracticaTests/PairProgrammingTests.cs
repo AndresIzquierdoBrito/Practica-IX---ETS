0f0906b3c7161bb83cf5edc74e3b167eb2ed5762
Andres Izquierdo <andres.izbri@gmail.com>
2023-03-31 13:53:06 +0100

    Completed all PairProgramming Tests

--- PracticaIX - Program and Tests/PracticaTests/PairProgrammingTests.cs
+++ PracticaIX - Program and Tests/PracticaTests/PairProgrammingTests.cs
@@ -5,17 +5,145 @@ namespace PracticaTests
     [TestClass]
     public class PairProgrammingTests
     {
-        
-        [TestMethod]
-        public void PersonaMasAltaTest()
-        {
-            Personas p = new Personas();
-            p.Nombre = "Pepe";
-            p.Altura = 1;
-            Personas[] arrP = { p };
-            string result = CSAlturas.personaMasAlta(arrP);
+        ///////////////////////////////////////// PersonaPorEncimaMedia /////////////////////////////////////
 
-            Assert.AreEqual(p.Nombre, result);
+        [TestMethod]
+        public void PersonaPorEncimaMedia_Caso1()   // Pasando una lista con MULTIPLES personas con la DIFERENTES alturas
+        {
+            Personas[] listaPersona = new Personas[]
+            {
+                new Personas { Nombre = "Persona 1", Altura = 1.60m },
+                new Personas { Nombre = "Persona 2", Altura = 1.80m },
+                new Personas { Nombre = "Persona 3", Altura = 1.60m },
+                new Personas { Nombre = "Persona 4", Altura = 1.90m },
+                new Personas { Nombre = "Persona 5", Altura = 1.75m },
+            };
+
+            string[] result = CSAlturas.PersonasPorEncimaMedia(listaPersona);
+
+            CollectionAssert.AreEqual(new[] { "Persona 2", "Persona 4", "Persona 5" }, result);
+        }
+        [TestMethod]
+        public void PersonaPorEncimaMedia_Caso2()   // Pasando una lista con MULTIPLES personas con la MISMA altura
+        {
+            Personas[] listaPersona = new Personas[]
+            {
+                new Personas { Nombre = "Persona 1", Altura = 1.70m },
+                new Personas { Nombre = "Persona 2", Altura = 1.70m },
+                new Personas { Nombre = "Persona 3", Altura = 1.70m },
+            };
+
+            string[] result = CSAlturas.PersonasPorEncimaMedia(listaPersona);
+            CollectionAssert.AreEqual(new string[] { }, result);
+        }
+
+        [TestMethod]
+        public void PersonaPorEncimaMedia_Caso3()   // Pasando una lista VACIA
+        {
+            Personas[] listaPersona = new Personas[0];
+            string[] result = CSAlturas.PersonasPorEncimaMedia(listaPersona);
+            CollectionAssert.AreEqual(new string[] { }, result);
+        }
+
+        [TestMethod]
+        public void PersonaPorEncimaMedia_Caso4() // Pasando una lista con UNA persona
+        {
+            Personas[] listaPersona = new Personas[]
+            {
+                new Personas { Nombre = "Persona 1", Altura = 1.80m }
+            };
+            string[] result = CSAlturas.PersonasPorEncimaMedia(listaPersona);
+            CollectionAssert.AreEqual(new string[] { }, result);
+        }
+
+        ///////////////////////////////////////// PersonaPorDebajoMedia /////////////////////////////////////
+
+        [TestMethod]
+        public void PersonaPorDebajoMedia_Caso1() // Pasando una lista con MULTIPLES personas con la DIFERENTES alturas
+        {
+            Personas[] listaPersona = new Personas[]
+            {
+                new Personas { Nombre = "Persona 1", Altura = 1.60m },
+                new Personas { Nombre = "Persona 2", Altura = 1.80m },
+                new Personas { Nombre = "Persona 3", Altura = 1.60m },
+                new Personas { Nombre = "Persona 4", Altura = 1.90m },
+                new Personas { Nombre = "Persona 5", Altura = 1.75m },
+            };
+
+            string[] result = CSAlturas.PersonasPorDebajoMedia(listaPersona);
+
+            CollectionAssert.AreEqual(new[] { "Persona 1", "Persona 3" }, result);
+        }
+        [TestMethod]
+        public void PersonaPorDebajoMedia_Caso2() // Pasando una lista con MULTIPLES personas con la MISMA altura
+        {
+            Personas[] listaPersona = new Personas[]
+            {
+                new Personas { Nombre = "Persona 1", Altura = 1.70m },
+                new Personas { Nombre = "Persona 2", Altura = 1.70m },
+                new Personas { Nombre = "Persona 3", Altura = 1.70m },
+            };
+
+            string[] result = CSAlturas.PersonasPorDebajoMedia(listaPersona);
+            CollectionAssert.AreEqual(new string[] { }, result);
+        }
+
+        [TestMethod]
+        public void PersonaPorDebajoMedia_Caso3() // Pasando una lista VACIA
+        {
+            Personas[] listaPersona = new Personas[0];
+            string[] result = CSAlturas.PersonasPorDebajoMedia(listaPersona);
+            CollectionAssert.AreEqual(new string[] { }, result);
+        }
+
+        [TestMethod]
+        public void PersonaPorDebajoMedia_Caso4() // Pasando una lista con UNA persona
+        {
+            Personas[] listaPersona = new Personas[]
+            {
+                new Personas { Nombre = "Persona 1", Altura = 1.80m }
+            };
+            string[] result = CSAlturas.PersonasPorDebajoMedia(listaPersona);
+            CollectionAssert.AreEqual(new string[] { }, result);
+        }
+
+        ///////////////////////////////////////// PersonaMasAlta /////////////////////////////////////
+
+
+        [TestMethod]
+        public void PersonaMasAlta_Caso1()  // Pasando una lista con UNA persona
+        {
+            Personas[] listaPersona = new Personas[]
+            {
+                new Personas { Nombre = "Persona 1", Altura = 1.80m }
+            };
+            string result = CSAlturas.personaMasAlta(listaPersona);
+
+            Assert.AreEqual("Persona 1", result);
+        }
+
+        [TestMethod]
+        public void PersonaMasAlta_Caso2() // Pasando una lista con MULTIPLES personaS
+        {
+            Personas[] listaPersona = new Personas[]
+            {
+                new Personas { Nombre = "Persona 1", Altura = 1.60m },
+                new Personas { Nombre = "Persona 2", Altura = 1.80m },
+                new Personas { Nombre = "Persona 3", Altura = 1.60m },
+                new Personas { Nombre = "Persona 4", Altura = 1.90m },
+                new Personas { Nombre = "Persona 5", Altura = 1.75m },
+            };
+            string result = CSAlturas.personaMasAlta(listaPersona);
+            
+            Assert.AreEqual("Persona 4", result);
+        }
+
+        [TestMethod]
+        public void PersonaMasAlta_Caso3() // Pasando una lista VACIA
+        {
+            Personas[] listaPersona = new Personas[0];
+
+            Assert.ThrowsException<IndexOutOfRangeException>(() => CSAlturas.personaMasAlta(listaPersona));
         }
     }
 }
